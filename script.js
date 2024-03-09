d46093b32048fb3d8c55d3af4ede9ebd3615e840
Pokdee <loselife61@gmail.com>
2024-03-09 22:36:22 +0700

    Project Finish

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const api = "https://api.quotable.io/quotes/random";
 const qText = document.querySelector(".quote-text");
 const qAuthor = document.querySelector(".quote-author");
+const btnNext = document.querySelector(".btn-new");
+const loader = document.querySelector(".loader");
 
-console.log(qText, qAuthor);
 const getQuote = async function () {
   const res = await fetch(api);
   const data = await res.json();
+  loader.classList.add("hide");
   let quote = {};
   quote.author = data[0].authorSlug;
   quote.content = data[0].content;
-  console.log(quote);
   qText.textContent = quote.content;
   qAuthor.textContent = quote.author;
 };
 
 getQuote();
+
+btnNext.addEventListener("click", () => {
+  loader.classList.remove("hide");
+  getQuote();
+});
